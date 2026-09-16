e8e999ce9193b742bfc3289feb653b89e0c01583
buinhattan10c1leloi-debug <buinhattan10c1leloi@gmail.com>
2026-09-16 19:44:11 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 function login(username, password) {
-  // Kiểm tra username rỗng hoặc chỉ chứa khoảng trắng tinh
+  // Kiểm tra username rỗng hoặc chỉ chứa khoảng trắng tinh thơm
   if (!username || username.trim() === "") {
     throw new Error("Username không được để trống");
   }
